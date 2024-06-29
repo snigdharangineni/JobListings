581bc7a177b8c1bbfb5c111d283ece21127de93f
snigdha <sunny.rangineni@gmail.com>
2024-06-28 22:26:24 -0500

    post a job to MongoDb

--- src/main/java/com/springboot/joblisting/controller/PostController.java
+++ src/main/java/com/springboot/joblisting/controller/PostController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,12 @@ public class PostController {
 	public List<Post> getAllPosts()
 	{
 		return repo.findAll();
+	}
+	
+	@PostMapping("/post")
+	public Post addPost(@RequestBody Post post)
+	{
+		return repo.save(post);
+		
 	}
 }
