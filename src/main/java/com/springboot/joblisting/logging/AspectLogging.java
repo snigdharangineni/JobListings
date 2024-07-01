588a10ebbfc36e0daa5f3ea6fa103573a7588339
snigdha <sunny.rangineni@gmail.com>
2024-07-01 17:03:50 -0500

    Adding seperate logging file to track the logging statements

--- src/main/java/com/springboot/joblisting/logging/AspectLogging.java
+++ src/main/java/com/springboot/joblisting/logging/AspectLogging.java
@@ -1,5 +1,8 @@
 package com.springboot.joblisting.logging;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -17,5 +20,26 @@ public class AspectLogging {
 	public void log()
 	{
 		log.info("get posts method called");
+	}
+	
+	//executes if an exception is thrown
+	@After("execution(public * com.springboot.joblisting.controller.PostController.getAllPosts())")
+	public void logAfter()
+	{
+		log.info("get posts method called");
+	}
+	
+	//executes only if the method is successful
+	@AfterReturning("execution(public * com.springboot.joblisting.controller.PostController.getAllPosts())")
+	public void logAfterReturning()
+	{
+		log.info("method is succesful");
+	}
+	
+	//executes only if the method ran into some issue
+	@AfterThrowing("execution(public * com.springboot.joblisting.controller.PostController.getAllPosts())")
+	public void logAfterThrowing()
+	{
+		log.info("issue");
 	}
 }
